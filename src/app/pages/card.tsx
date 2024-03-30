0600d72f562966a43fcfe75deb3d5a63c0b780af
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2024-03-30 15:59:23 -0300

    Routing

---
+++ src/app/pages/card.tsx
@@ -0,0 +1,5 @@
+const Card: React.FC = () => {
+  return <h1>oi</h1>
+}
+
+export default Card;
--- src/components/menu/sidebar/MenuSidebar.tsx
+++ src/components/menu/sidebar/MenuSidebar.tsx
@@ -1,38 +1,10 @@
 "use client"
 
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { MdExitToApp, MdKeyboardArrowDown, MdKeyboardControlKey, MdOutlineAccountBalanceWallet, MdOutlineAutoGraph, MdOutlineSettings, MdPriceCheck } from 'react-icons/md';
 import './style.css';
-
-const menus = [
-  {
-    icon: <MdOutlineAccountBalanceWallet fontSize={20} />,
-    label: 'Cadastro',
-    key: 'mn-add',
-    sub_menus: [
-      { label: 'Contas fixas' },
-      { label: 'Cartões' },
-    ],
-  },
-  {
-    icon: <MdPriceCheck fontSize={20} />,
-    label: 'Contas a pagar',
-    key: 'mn-bills-to-pay',
-    sub_menus: [
-      { label: 'Digitação' },
-    ],
-  },
-  {
-    icon: <MdOutlineAutoGraph fontSize={20} />,
-    label: 'Relatório',
-    key: 'mn-report',
-  },
-  {
-    icon: <MdOutlineSettings fontSize={20} />,
-    label: 'Configuração',
-    key: 'mn-setting',
-  },
-];
+import Link from 'next/link';
 
 interface ISubMenuStyle {
   [key: string]: boolean;
@@ -40,6 +12,52 @@ interface ISubMenuStyle {
 
 function MenuSidebar() {
   const [subMenusOpened, setSubMenusOpened] = useState({} as ISubMenuStyle);
+  const router = useRouter();
+
+  const menus = [
+    {
+      icon: <MdOutlineAccountBalanceWallet fontSize={20} />,
+      label: 'Cadastro',
+      key: 'mn-add',
+      sub_menus: [
+        {
+          key: 'fixed-account',
+          label: 'Contas fixas',
+          onClick: () => {
+            router.push('/pages/card');
+          },
+        },
+        {
+          key: 'mn-card',
+          label: 'Cartões',
+          onClick: () => {
+            console.log('cliquei auqui nos cartões');
+          },
+        },
+      ],
+    },
+    {
+      icon: <MdPriceCheck fontSize={20} />,
+      label: 'Contas a pagar',
+      key: 'mn-bills-to-pay',
+      sub_menus: [
+        { key: 'mn-entry', label: 'Digitação' },
+      ],
+    },
+    {
+      icon: <MdOutlineAutoGraph fontSize={20} />,
+      label: 'Relatório',
+      key: 'mn-report',
+    },
+    {
+      icon: <MdOutlineSettings fontSize={20} />,
+      label: 'Configuração',
+      key: 'mn-setting',
+      onClick: () => {
+        console.log('cliquei nas configurações');
+      },
+    },
+  ];
 
   function handleClickMenu(event: any, ownerMenuSelected: string) {
     event.preventDefault();
@@ -57,11 +75,13 @@ function MenuSidebar() {
     if (subMenusOpened[keyOwnerMenu]) {
       return (
         <ul>
-          {submenus?.map((sm: any, idxsm: number) =>
-            <li key={`submenu-${idxsm}`} className='submenu'>
-              <span>{sm.label}</span>
-            </li>
-          )}
+          {
+            submenus?.map((sm: any) =>
+              <li key={sm.key} className='submenu' onClick={() => sm?.onClick?.()}>
+                <span>{sm.label}</span>
+              </li>
+            )
+          }
           <div className='divider' />
         </ul>
       );
@@ -83,29 +103,37 @@ function MenuSidebar() {
     <div className='menu-container' onMouseLeave={handleMenuMouseLeave}>
       <div className='menu'>
         {
-          menus.map(({ key, icon, label, sub_menus }) =>
-            <ul>
-              <li key={key} onClick={(e) => handleClickMenu(e, key)}>
-                <div>{icon}</div>
-                <div className='container-label'>
-                  <span>{label}</span>
-                  <GetSufixIcon
-                    keyOwnerMenu={key}
-                    hasSubMenus={(sub_menus?.length || 0) > 0}
-                  />
-                </div>
-              </li>
-              <SubMenuList
-                submenus={sub_menus}
-                keyOwnerMenu={key}
-              />
-            </ul>
-          )
+          menus.map(({ key, icon, label, sub_menus, onClick }) => {
+            const hasSubMenus = (sub_menus?.length || 0) > 0;
+            const onClickMenu = (e: any) => hasSubMenus ? handleClickMenu(e, key) : onClick?.();
+
+            return (
+              <ul>
+                <li key={key} onClick={onClickMenu}>
+                  <div>{icon}</div>
+                  <div className='container-label'>
+                    <span>{label}</span>
+                    <GetSufixIcon
+                      keyOwnerMenu={key}
+                      hasSubMenus={hasSubMenus}
+                    />
+                  </div>
+                </li>
+                <SubMenuList
+                  submenus={sub_menus}
+                  keyOwnerMenu={key}
+                />
+              </ul>
+            )
+          })
         }
       </div>
       <ul className='menu container-menu-footer'>
-        <li>
+        <li key={'mn-logout'}>
           <div>
+            <Link href={'card'}>
+              <button>teste</button>
+            </Link>
             <MdExitToApp fontSize={20} />
           </div>
           <span>Sair</span>
