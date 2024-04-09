a4366df1dbee2d626e88c742c86480c583104d13
hugodesenv <hugosouzadev@hotmail.com>
2024-04-08 23:56:09 -0300

    Calendario

--- src/components/calendar/MyCalendar.tsx
+++ src/components/calendar/MyCalendar.tsx
@@ -1,53 +1,86 @@
-'use client';
+"use client";
 
-import { DateUtils } from '../../lib/dateUtils';
+import { DateUtils } from "../../lib/dateUtils";
 
 interface IDayOfWeek {
   label: string;
-};
+}
+
+interface IElements {
+  day: number;
+  month: number;
+  year: number;
+  outOfMonth: boolean;
+  children: any[];
+}
 
 const columns: IDayOfWeek[] = [
-  { label: 'Dom' },
-  { label: 'Seg' },
-  { label: 'Ter' },
-  { label: 'Qua' },
-  { label: 'Qui' },
-  { label: 'Sex' },
-  { label: 'Sab' },
+  { label: "Dom" },
+  { label: "Seg" },
+  { label: "Ter" },
+  { label: "Qua" },
+  { label: "Qui" },
+  { label: "Sex" },
+  { label: "Sab" },
 ];
 
-function handleDays() {
-  const currentDate = new Date();
+const MAX_SIZE = 7 * 6;
 
-  const indexFirstDay = DateUtils.getWeekIndex(
-    currentDate.getFullYear(),
-    currentDate.getMonth(),
-    1
+/**
+ * Nessa função, eu preciso saber qual é o dia que estamos, e iniciamos
+ * no dia 1. Com isso, obtemos em qual dia da semana o numero 1 se refere,
+ * segunda, terça, quarta etc. Com esse indice, eu subtraio do primeiro dia
+ * do mês, para obter os dias anteriores a ser mostrado no calendario.
+ * A partir daí, eu monto meu objeto resultado, informando o dia.
+ * Eu terei 42 espaço (7 dias X 6 semanas).
+ */
+function getCalendarDays() {
+  let dataResult = [] as IElements[];
+
+  let startDate = new Date();
+  startDate.setDate(1);
+
+  const currentMonth = startDate.getMonth();
+  const indexFirstDayOfMonth = DateUtils.getWeekIndex(
+    startDate.getFullYear(),
+    startDate.getMonth() + 1,
+    startDate.getDate()
   );
 
-  const startDate = new Date();
-  console.log(startDate);
+  startDate.setDate(startDate.getDate() - indexFirstDayOfMonth);
+
+  for (let i = 0; i < MAX_SIZE; i++) {
+    const dayObject: IElements = {
+      children: [],
+      day: startDate.getDate(),
+      month: startDate.getMonth() + 1,
+      year: startDate.getFullYear(),
+      outOfMonth: startDate.getMonth() !== currentMonth,
+    };
+
+    dataResult.push(dayObject);
+    startDate.setDate(startDate.getDate() + 1);
+  }
+
+  return dataResult;
+}
+
+function DaysComponent() {
+  const daysObject = getCalendarDays();
+  return <td>daysObject</td>
 }
 
 export default function MyCalendar() {
-  handleDays();
-
   return (
     <table border={1}>
       <thead>
         <tr>
-          {columns.map(({ label }) => <th>{label}</th>)}
+          {columns.map(({ label }) => (
+            <th>{label}</th>
+          ))}
         </tr>
       </thead>
-      <tbody>
-        {
-
-        }
-        <tr>
-
-        </tr>
-
-      </tbody>
+      <DaysComponent />
     </table>
-  )
+  );
 }
--- src/lib/dateUtils.ts
+++ src/lib/dateUtils.ts
@@ -5,7 +5,7 @@ export class DateUtils {
     const d = pDay.toString().padStart(2, '0');
 
     const firstDay = new Date(`${y}-${m}-${d} 00:00:00`);
-    
+
     return firstDay.getDay();
   }
 }
